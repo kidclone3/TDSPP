99553b2596cde450ec40e0a0a2c65fbcf32b31d6
kidclone3 <kid.clone.3@gmail.com>
2024-05-11 17:43:14 +0700

    ref: add comments for outputs

--- Output.h
+++ Output.h
@@ -10,16 +10,16 @@
 using namespace std;
 
 struct Output {
-    bool md = 0;
-    bool ddd = 0;
-    int bpExplored = 0;
-    double runtime = 0;
-    double add_runtime = 0;
-    double sp_runtime = 0;
-    int arc_total = 0;
-    int subPathTotal = 1;
-    double optVal = 0;
-    int iter = 0;
+    bool md = 0; // Flag for MD state, default is false
+    bool ddd = 0; // Flag for DDD state, default is false
+    int bpExplored = 0; // Number of BP explored
+    double runtime = 0; // Total runtime
+    double add_runtime = 0; // Additional runtime
+    double sp_runtime = 0; // SP runtime
+    int arc_total = 0; // Total number of arcs in solution
+    int subPathTotal = 1; // Total number of subpaths in solution
+    double optVal = 0; // Optimal objective value
+    int iter = 0; // Number of iterations
 
     void writeOutputCSV(string &filename);
 };
--- SummaryOutput.h
+++ SummaryOutput.h
@@ -13,31 +13,31 @@ using namespace std;
 
 class SummaryOutput {
 public:
-    bool md;
-    int n;
-    int T;
-    int gtype;
-    int ttype;
-    int numSeed;
-    int bpExplored = 0;
-    double runtime = 0;
-    double enumtime = 0;
-    int arc_total = 0;
-    int subpath_total = 0;
-    double avg_bpExplored = 0;
-    int bp_total = 0;
-    double percent_bp = 0;
-    double avg_runtime = 0;
-    double avg_enumtime = 0;
-    double percent_time = 0;
-    double gavg_percent_time = 1;
-    double avg_arc = 0;
-    double avg_subpath = 0;
-    double total_add_runtime = 0;
-    double total_sp_runtime = 0;
-    double avg_add_runtime = 0;
-    double avg_sp_runtime = 0;
-    int iter = 0;
+    bool md; // Flag for MD state, default is false
+    int n; // Number of nodes
+    int T; // time horizon
+    int gtype; // Graph type
+    int ttype; // Travel time type
+    int numSeed; // seed number
+    int bpExplored = 0; // Number of BP explored
+    double runtime = 0; // Total runtime for DDD
+    double enumtime = 0; // Total runtime for enumerate method
+    int arc_total = 0; // Total number of arcs in solution
+    int subpath_total = 0; // Total number of subpaths in solution
+    double avg_bpExplored = 0; // Average number of BP explored
+    int bp_total = 0; // Total number of BP explored
+    double percent_bp = 0; // Percentage of BP explored
+    double avg_runtime = 0; // Average runtime
+    double avg_enumtime = 0; // Average runtime for enumerate method
+    double percent_time = 0; // Percentage of runtime vs enumerate method
+    double gavg_percent_time = 1; // Geometric mean of percentage of runtime vs enumerate method
+    double avg_arc = 0; // Average number of arcs in solution
+    double avg_subpath = 0; // Average number of subpaths in solution
+    double total_add_runtime = 0; // Total additional runtime
+    double total_sp_runtime = 0; // Total SP runtime
+    double avg_add_runtime = 0; // Average additional runtime
+    double avg_sp_runtime = 0; // Average SP runtime
+    int iter = 0; // Number of iterations
     string filename;
 
     SummaryOutput(string filename, int n, int T, int gtype, int ttype, int numseed, bool md);
